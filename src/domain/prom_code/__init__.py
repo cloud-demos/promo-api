146ff20db0986a8bb1f54e0f2d3469b95188537b
José Albert Cruz Almaguer <jalbertcruz@gmail.com>
2018-11-07 11:11:08 -0500

    Creation codes implementation added

--- src/domain/prom_code/__init__.py
+++ src/domain/prom_code/__init__.py
@@ -8,8 +8,16 @@ GeneratePromoCodeResult = Enum('GeneratePromoCodeResult', 'EventDoNotExists Ok')
 
 
 def generate_promo_code(event_id, data={}):
+    event = Event.query.get(event_id)
+    if not event:
+        return GeneratePromoCodeResult.EventDoNotExists, None
 
-    pcode = PromCode()
+    pcode = PromCode(
+        credit=data.get("credit", config.DEFAULT_CREDIT),
+        radius=data.get("radius", config.DEFAULT_RADIUS),
+        expiration_time=data.get("expiration_time", utils.get_default_expiration_time()),
+        code=utils.string_generator(config.CODE_LENGTH),
+    )
 
     db.session.add(pcode)
     db.session.commit()
